bn0/flask-restplus-server-example

commit e8c64ce33781e8edcc132061eab3f9a3343f8952

Fix PEP8 infractions

app/modules/users/permissions/__init__.py
@@ -43,13 +43,15 @@ class Permission(BasePermission):
     @classmethod
     def get_query_class(cls):
         """
-        Returns extended BaseQuery class for flask_sqlalchemy model to provide get_or_403 method
+        Returns extended BaseQuery class for flask_sqlalchemy model
+        to provide get_or_403 method
 
         Example:
         >>> DataTransformation(db.Model):
         ...     query_class = OwnerRolePermission.get_query_class()
         """
-        return lambda *args, **kwargs: PermissionExtendedQuery(cls, *args, **kwargs)
+        return lambda *args, **kwargs: 
+            PermissionExtendedQuery(cls, *args, **kwargs)
 
 
 class PasswordRequiredPermissionMixin(object):
@@ -123,8 +125,9 @@ class AdminRolePermission(PasswordRequiredPermissionMixin, RolePermission):
 
     def rule(self):
         return (
-            rules.InternalRoleRule()
-            | (rules.AdminRoleRule() & super(AdminRolePermission, self).rule())
+            rules.InternalRoleRule() | (
+                rules.AdminRoleRule() & super(AdminRolePermission, self).rule()
+            )
         )
 
 
@@ -137,7 +140,8 @@ class InternalRolePermission(RolePermission):
         return rules.InternalRoleRule()
 
 
-class SupervisorRolePermission(PasswordRequiredPermissionMixin, RolePermission):
+class SupervisorRolePermission(
+        PasswordRequiredPermissionMixin, RolePermission):
     """
     Supervisor/Admin may execute this action.
     """
@@ -155,13 +159,11 @@ class SupervisorRolePermission(PasswordRequiredPermissionMixin, RolePermission):
 
     def rule(self):
         return (
-            rules.InternalRoleRule()
-            | (
+            rules.InternalRoleRule() | (
                 (
-                    rules.AdminRoleRule()
-                    | rules.SupervisorRoleRule(obj=self._obj)
-                )
-                & super(SupervisorRolePermission, self).rule()
+                    rules.AdminRoleRule() |
+                    rules.SupervisorRoleRule(obj=self._obj)
+                ) & super(SupervisorRolePermission, self).rule()
             )
         )
 
@@ -184,13 +186,11 @@ class OwnerRolePermission(PasswordRequiredPermissionMixin, RolePermission):
 
     def rule(self):
         return (
-            rules.InternalRoleRule()
-            | (
+            rules.InternalRoleRule() | (
                 (
-                    rules.AdminRoleRule()
-                    | rules.OwnerRoleRule(obj=self._obj)
-                    | rules.SupervisorRoleRule(obj=self._obj)
-                )
-                & super(OwnerRolePermission, self).rule()
+                    rules.AdminRoleRule() |
+                    rules.OwnerRoleRule(obj=self._obj) |
+                    rules.SupervisorRoleRule(obj=self._obj)
+                ) & super(OwnerRolePermission, self).rule()
             )
         )

app/__init__.py
@@ -16,6 +16,7 @@ CONFIG_NAME_MAPPER = {
     'local': 'local_config.LocalConfig',
 }
 
+
 def create_app(flask_config_name=None, **kwargs):
     """
     Entry point to the Flask RESTful Server application.
@@ -30,8 +31,9 @@ def create_app(flask_config_name=None, **kwargs):
     else:
         if env_flask_config_name:
             assert env_flask_config_name == flask_config_name, (
-                "FLASK_CONFIG environment variable (\"%s\") and flask_config_name argument "
-                "(\"%s\") are both set and are not the same." % (
+                "FLASK_CONFIG environment variable (\"%s\") and "
+                "flask_config_name argument (\"%s\") are both set "
+                "and are not the same." % (
                     env_flask_config_name,
                     flask_config_name
                 )
@@ -42,10 +44,11 @@ def create_app(flask_config_name=None, **kwargs):
     except ImportError:
         if flask_config_name == 'local':
             app.logger.error(
-                "You have to have `local_config.py` or `local_config/__init__.py` in order to use "
-                "the default 'local' Flask Config. Alternatively, you may set `FLASK_CONFIG` "
-                "environment variable to one of the following options: development, production, "
-                "testing."
+                "You have to have `local_config.py` or "
+                "`local_config/__init__.py` in order to use the default "
+                "'local' Flask Config. Alternatively, you may set "
+                "`FLASK_CONFIG` environment variable to one of the following "
+                "options: development, production, testing."
             )
             sys.exit(1)
         raise

app/extensions/__init__.py
@@ -48,13 +48,14 @@ def init_app(app):
     Application extensions initialization.
     """
     for extension in (
-            cross_origin_resource_sharing,
-            db,
-            login_manager,
-            marshmallow,
-            api,
-            oauth2,
+        cross_origin_resource_sharing,
+        db,
+        login_manager,
+        marshmallow,
+        api,
+        oauth2,
     ):
         extension.init_app(app)
 
-    app.extensions['migrate'] = AlembicDatabaseMigrationConfig(db, compare_type=True)
+    app.extensions['migrate'] = \
+        AlembicDatabaseMigrationConfig(db, compare_type=True)

app/extensions/api/__init__.py
@@ -13,24 +13,29 @@ from .namespace import Namespace
 from .http_exceptions import abort
 
 
-api_v1 = Api( # pylint: disable=invalid-name
+api_v1 = Api(  # pylint: disable=invalid-name
     version='1.0',
     title="Flask-RESTplus Example API",
     description=(
-        "It is a [real-life example RESTful API server implementation using Flask-RESTplus]"
+        "It is a [real-life example RESTful API server implementation using "
+        "Flask-RESTplus] "
         "(https://github.com/frol/flask-restplus-server-example).\n\n"
         "This demo features:\n"
-        "* Self-documented RESTful API server using autogenerated OpenAPI specifications;\n"
-        "* OAuth2 Password Flow (Resource Owner Password Credentials Grant) support;\n"
+        "* Self-documented RESTful API server using autogenerated OpenAPI "
+        "specifications;\n"
+        "* OAuth2 Password Flow (Resource Owner Password Credentials Grant) "
+        "support;\n"
         "* Role-based permission system (it is also auto-documented);\n"
         "* PATCH method handled accordingly to RFC 6902;\n"
         "* 95+% code coverage.\n\n"
         "## Explore the demo\n\n"
-        "I suggest you start with signing up a new user. To do so, use `POST /users/` endpoint "
+        "I suggest you start with signing up a new user. To do so, use "
+        "`POST /users/` endpoint "
         "with `recaptcha_key=\"secret_key\"`.\n\n"
-        "You will need to know the API Client ID to authenticate, so here it is: "
-        "`documentation`. Sometimes (e.g. for token refreshing) you might need API "
-        "Client Secret: `KQ()SWK)SQK)QWSKQW(SKQ)S(QWSQW(SJ*HQ&HQW*SQ*^SSQWSGQSG`.\n\n"
+        "You will need to know the API Client ID to authenticate, so "
+        "here it is: `documentation`. Sometimes (e.g. for token refreshing) "
+        "you might need API Client Secret: "
+        "`KQ()SWK)SQK)QWSKQW(SKQ)S(QWSQW(SJ*HQ&HQW*SQ*^SSQWSGQSG`.\n\n"
         "There are also two built-in users:\n"
         "* `root` (administrator with all permissions) with password `q`\n"
         "* `user` (regular user) with password `w`\n"
@@ -45,7 +50,8 @@ def serve_swaggerui_assets(path):
     if not current_app.debug:
         import warnings
         warnings.warn(
-            "/swaggerui/ is recommended to be served by public-facing server (e.g. NGINX)"
+            "/swaggerui/ is recommended to be served by public-facing server "
+            "(e.g. NGINX)"
         )
     from flask import send_from_directory
     return send_from_directory('../static/', path)

app/modules/__init__.py
@@ -14,4 +14,5 @@ def init_app(app, **kwargs):
     from importlib import import_module
 
     for module_name in app.config['ENABLED_MODULES']:
-        import_module('.%s' % module_name, package=__name__).init_app(app, **kwargs)
+        import_module(
+            '.%s' % module_name, package=__name__).init_app(app, **kwargs)

app/modules/auth/__init__.py
@@ -20,6 +20,7 @@ def load_user_from_request(request):
             user = oauth.user
     return user
 
+
 def init_app(app, **kwargs):
     # pylint: disable=unused-argument
     """
@@ -29,8 +30,14 @@ def init_app(app, **kwargs):
     login_manager.request_loader(load_user_from_request)
 
     # Register OAuth scopes
-    api_v1.add_oauth_scope('auth:read', "Provide access to auth details")
-    api_v1.add_oauth_scope('auth:write', "Provide write access to auth details")
+    api_v1.add_oauth_scope(
+        'auth:read',
+        "Provide access to auth details"
+    )
+    api_v1.add_oauth_scope(
+        'auth:write',
+        "Provide write access to auth details"
+    )
 
     # Touch underlying modules
     from . import models, views, resources

app/modules/teams/__init__.py
@@ -12,8 +12,14 @@ def init_app(app, **kwargs):
     """
     Init teams module.
     """
-    api_v1.add_oauth_scope('teams:read', "Provide access to team details")
-    api_v1.add_oauth_scope('teams:write', "Provide write access to team details")
+    api_v1.add_oauth_scope(
+        'teams:read',
+        "Provide access to team details"
+    )
+    api_v1.add_oauth_scope(
+        'teams:write',
+        "Provide write access to team details"
+    )
 
     # Touch underlying modules
     from . import models, resources

app/modules/users/__init__.py
@@ -12,8 +12,14 @@ def init_app(app, **kwargs):
     """
     Init users module.
     """
-    api_v1.add_oauth_scope('users:read', "Provide access to user details")
-    api_v1.add_oauth_scope('users:write', "Provide write access to user details")
+    api_v1.add_oauth_scope(
+        'users:read',
+        "Provide access to user details"
+    )
+    api_v1.add_oauth_scope(
+        'users:write',
+        "Provide write access to user details"
+    )
 
     # Touch underlying modules
     from . import models, resources

commit 11b55105473dd00606fa0a1bccabdb7ab51ce1ed

Fix lambda syntax error that was introduced by previous commit (e8c64ce)

app/modules/users/permissions/__init__.py
@@ -50,8 +50,8 @@ class Permission(BasePermission):
         >>> DataTransformation(db.Model):
         ...     query_class = OwnerRolePermission.get_query_class()
         """
-        return lambda *args, **kwargs: 
-            PermissionExtendedQuery(cls, *args, **kwargs)
+        return lambda *args, **kwargs: PermissionExtendedQuery(
+            cls, *args, **kwargs)
 
 
 class PasswordRequiredPermissionMixin(object):